b82ff1cea2cbf18e3ae95d228f9dec681b809d9e
sapphi-red <green@sapphi.red>
2021-01-09 21:17:13 +0900

    Init

---
+++ README.md
@@ -0,0 +1,89 @@
+# vite-plugin-chemiscript
+[![MIT License](http://img.shields.io/badge/license-MIT-blue.svg?style=flat)](LICENSE)  
+
+A plugin that auto-reactifies your script.
+
+> **Vite v2 is only supported, Vite v1 will never be supported**
+
+Powered by [`vue-chemistry`](https://github.com/antfu/vue-chemistry).
+
+**This project is seeking for a solution to the curious syntax and will not be developed untill it is solved.**
+See the example for more details.
+
+## Install
+```shell
+npm i -D sapphi-red/vite-plugin-chemiscript # yarn add -D sapphi-red/vite-plugin-chemiscript
+```
+
+Add plugin in `vite.config.js`.
+```js
+import { chemiscript } from 'vite-plugin-chemiscript'
+
+export default {
+  plugins: [
+    chemiscript()
+  ]
+}
+```
+
+## Example
+```vue
+<template>
+  <div @click="increment">
+    {{ a }}
+    {{ b }}
+  </div>
+</template>
+
+<script chemi>
+export default {
+  setup() {
+    let a = 0
+    const increment = () => {
+      a = a + 1
+    }
+    const b = a + 5
+    return { a, b, increment }
+  }
+}
+</script>
+```
+
+```vue
+<template>
+  <div @click="increment">
+    {{ a }}
+    {{ b }}
+  </div>
+</template>
+
+<script chemi>
+import { sum } from 'vue-chemistry/math'
+import { reactify, set } from '@vueuse/shared'
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    let a = ref(0)
+    const increment = () => {
+      set(a, a.value + 1)
+    }
+    const b = sum(a, 5)
+    return { a, b, increment }
+  }
+}
+</script>
+```
+
+### What is curious?
+```ts
+a = a + 1
+```
+here this assigns the result of `a.value + 1`
+
+```ts
+const b = a + 5
+```
+here this assigns the computed value
+
+It has different behaviors with the same syntax.
---
+++ src/index.ts
@@ -0,0 +1,151 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import type { TransformHook } from 'rollup'
+import type { Plugin } from 'vite'
+import { parse } from '@vue/compiler-sfc'
+import { parse as scriptParse } from '@babel/parser'
+import {
+  ObjectExpression,
+  BlockStatement,
+  Statement,
+  Expression
+} from '@babel/types'
+import MagicString from 'magic-string'
+
+export const chemiscript = (): Plugin => {
+  const transform: TransformHook = function (code, id) {
+    if (!id.endsWith('.vue')) {
+      return null
+    }
+    const { descriptor, errors } = parse(code)
+    if (errors.length > 0) {
+      return null
+    }
+    if (!descriptor.script || !descriptor.script.attrs.chemi) {
+      return null
+    }
+
+    const input = descriptor.script.content
+
+    const s = new MagicString(input)
+    const ast = scriptParse(input, {
+      sourceType: 'module'
+    }).program.body
+
+    let exportObjectNode: ObjectExpression | null = null
+    ast.forEach(node => {
+      if (node.type !== 'ExportDefaultDeclaration') return
+      const decl = node.declaration
+      if (decl.type === 'ObjectExpression') {
+        exportObjectNode = decl
+        return
+      }
+
+      if (decl.type === 'CallExpression') {
+        if (decl.arguments.length > 0) {
+          const arg = decl.arguments[0]
+          if (arg?.type === 'ObjectExpression') {
+            exportObjectNode = arg
+          }
+        }
+      }
+    })
+
+    if (!exportObjectNode) {
+      return
+    }
+    const node = exportObjectNode as ObjectExpression
+    let bodyNode: BlockStatement | null = null
+    node.properties.forEach(prop => {
+      if (prop.type !== 'ObjectMethod') return
+      if (prop.key.type !== 'Identifier' || prop.key.name !== 'setup') return
+
+      bodyNode = prop.body
+    })
+    if (!bodyNode) {
+      return
+    }
+
+    s.prepend("import { ref } from 'vue'\n")
+    s.prepend("import { reactify, set } from '@vueuse/shared'\n")
+    s.prepend("import { sum } from 'vue-chemistry/math'\n")
+    s.prepend('\n')
+
+    const rewriteStatements = (
+      node: Statement | Expression,
+      reactive = true
+    ) => {
+      if (node.type === 'VariableDeclaration') {
+        node.declarations.forEach(decl => {
+          const init = decl.init
+          if (init && init.start && init.end) {
+            if (
+              init.type === 'ArrowFunctionExpression' ||
+              init.type === 'FunctionExpression'
+            ) {
+              if (init.body.type === 'BlockStatement') {
+                init.body.body.forEach(node => rewriteStatements(node))
+              }
+              // s.prependLeft(init.start, 'reactify(')
+              // s.appendRight(init.end, ')')
+            } else if (init.type === 'NumericLiteral') {
+              s.prependLeft(init.start, 'ref(')
+              s.appendRight(init.end, ')')
+            } else {
+              rewriteStatements(init)
+            }
+          }
+        })
+      } else if (node.type === 'ExpressionStatement') {
+        const exp = node.expression
+        rewriteStatements(exp)
+      } else if (node.type === 'AssignmentExpression') {
+        const lhs = node.left
+        const rhs = node.right
+        rewriteStatements(rhs, false)
+        s.prependLeft(lhs.start!, 'set(')
+        s.overwrite(lhs.end!, rhs.start!, ', ')
+        s.appendRight(rhs.end!, ')')
+      } else if (node.type === 'BinaryExpression') {
+        const lhs = node.left
+        const rhs = node.right
+        if (reactive) {
+          if (node.operator === '+') {
+            s.prependRight(lhs.start!, 'sum(')
+          }
+          s.overwrite(lhs.end!, rhs.start!, ', ')
+          s.appendLeft(rhs.end!, ')')
+        } else {
+          if (lhs.type !== 'PrivateName') {
+            rewriteStatements(lhs)
+          }
+          rewriteStatements(rhs)
+        }
+      } else if (node.type === 'Identifier') {
+        s.appendLeft(node.end!, '.value')
+      }
+    }
+
+    const n = bodyNode as BlockStatement
+    n.body.forEach(node => rewriteStatements(node))
+
+    const cs = new MagicString(code)
+    cs.overwrite(
+      descriptor.script.loc.start.offset,
+      descriptor.script.loc.end.offset,
+      s.toString()
+    )
+
+    console.log(cs.toString())
+
+    return {
+      code: cs.toString(),
+      map: cs.generateMap()
+    }
+  }
+
+  return {
+    name: 'chemiscript',
+    enforce: 'pre',
+    transform
+  }
+}
